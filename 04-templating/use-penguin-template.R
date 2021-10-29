f53c90a01c421eadde97ce497ada4171bf86847e
Zhaoshan-Duan <joshua.duan47@gmail.com>
2021-10-28 19:40:27 -0700

    finish up presentation

--- 04-templating/use-penguin-template.R
+++ 04-templating/use-penguin-template.R
@@ -10,7 +10,6 @@ use_penguin_template <- function(species, long_prose){
   )
 }
 
-
 species <- "Adelie"
 
 long_prose <- "### Mating\n\nThe mating season begins with the Antarctic spring in October. The penguins create nests by piling little stones in circles. Once the egg is laid in December the parents take turns incubating the egg and going to hunt. The parent that stays behind does not eat during their turn with the egg. Once the hatched chick is about 3 weeks old both parents will abandon it, returning to the sea to hunt. The downy chicks gather into a group called a crèche to keep each other warm. They will start to hunt at about 9 weeks old once their down has been replaced with waterproof feathers."
